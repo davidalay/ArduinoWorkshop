57b688ef305512bf0a00f42de3c28920183618dc
davidalay <davidalay561@gmail.com>
2020-01-18 16:29:46 -0800

    split code into workshop and answer key version

--- Spaceship__InterfaceV1.3_Answer_Key_/Spaceship__InterfaceV1.3_Answer_Key_.ino
+++ Spaceship__InterfaceV1.3_Answer_Key_/Spaceship__InterfaceV1.3_Answer_Key_.ino
@@ -1,14 +1,23 @@
-//Include Libraries Here
-
-//Declare Variables here
-
-
+#include <Servo.h>
+int switchState = 0;
+int switchStateTwo = 0;
+int instructions = 1; // Instructions will print while this variable set to 1.
+int laserPower = 0; // variable to store the laser power data.
+int buttonCounter = 0; // Sets a counter for button 2 presses to zero.
+int enginePosition = 0;    // variable to store the servo position.
+Servo galacticEngine;  // create servo object to control our space engine.
 
 void setup() {
-  //Define pins 3, 4, 5, 6 ,7 9 as output
-
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(9,OUTPUT);
   //DEFINE PINS 2, 8 AND A0 AS INPUT
-
+  pinMode(2, INPUT);
+  pinMode(8, INPUT);
+  pinMode(A0, INPUT);
   galacticEngine.attach(10);  // attaches the servo on pin 10 to the servo object
 }
 
@@ -23,31 +32,35 @@ void loop() {
   //*********************************************************************
   // WRITE START UP MESSAGE HERE (REMEMBER THAT WE ONLY WANT THIS TO PRINT ONCE)
   //*********************************************************************
-  if (????)
+  if (instructions == 1)
   {
-   //Start up message goes here
-    instructions = ??;
+    Serial.print("Starting command board \n");
+    delay(1000);
+    Serial.print("Startup complete\n");
+    instructions = 0;
   }
 
   //****************************************************************************
   // This section of code uses an if-else statement to read button 1. When the button is pressed, it turns on the lights.
   //****************************************************************************
 
-  if (switchState == ??) { // the button is not pressed
+  if (switchState == LOW) { // the button is not pressed
     //***************************************************************
     //TURN THE GREEN LED ON, AND BOTH RED LEDS OFF
     //***************************************************************
-    
+    digitalWrite(5, HIGH);
+    digitalWrite(4, LOW);
+    digitalWrite(3, LOW);
   }
 
   else { // the button is pressed
     Serial.print("Performing Console Lighting Test\n");
-    digitalWrite(5,??);
-    digitalWrite(4, ??);
+    digitalWrite(5,LOW);
+    digitalWrite(4, HIGH);
     delay(500);
 
-    digitalWrite(4,??);
-    digitalWrite(3,??);
+    digitalWrite(4,LOW);
+    digitalWrite(3,HIGH);
     delay(500);
 
   } // This brace the if-else statement
@@ -65,7 +78,7 @@ void loop() {
   }
 
   else { // the button is pressed. We then increment the counter
-    //incriment counter here
+    buttonCounter ++;
     Serial.print("Button pushed\n");
   }
 
@@ -87,13 +100,15 @@ void loop() {
     //****************************************************************
     for(int i=0; i<=255; i=i+5)
     {
-       //cycle through PWM values and write to serial
+        analogWrite(9, (255-i));
+        Serial.print(i);
+        Serial.print("\n");
         delay(100);
     }
     
     delay(1000);
     analogWrite(9, LOW);
-    buttonCounter=0; // Reset the counter to 0. In the next step we change this to "buttonCounter++;" This will add 1 to the counter.
+    buttonCounter++; // Reset the counter to 0. In the next step we change this to "buttonCounter++;" This will add 1 to the counter.
 
   } // This brace closes the if statement
 
@@ -126,33 +141,37 @@ void loop() {
 
   // If the counter is equal to 5, the button has been pressed 3 times. Engage the engines.
 
-  else if (buttonCounter == 5) {
-    Serial.print("Warming Up Warp Engines - ");
+//  else if (buttonCounter == 5) {
+  //  Serial.print("Warming Up Warp Engines - ");
 
-    for (//CYCLE FROM 0 TO 180 FOR ENGINEPOSITION) // goes from 0 degrees to 180 degrees in steps of 5 degrees
-      { 
-      galacticEngine.write(enginePosition); // tell servo to go to position stored in variable
+//    for (//CYCLE FROM 0 TO 180 FOR ENGINEPOSITION) { // goes from 0 degrees to 180 degrees in steps of 5 degrees
+//      galacticEngine.write(enginePosition); // tell servo to go to position stored in variable
+//
+//      // Blink Lights during Engine Test
+//      digitalWrite(3, LOW);
+//      digitalWrite(4, LOW);
+//      digitalWrite(5, HIGH);
+//      delay(35);
+//      digitalWrite(3, LOW);
+//      digitalWrite(4, HIGH);
+//      digitalWrite(5, HIGH);
+//      delay(35);
+//      digitalWrite(3, HIGH);
+//      digitalWrite(4, HIGH);
+//      digitalWrite(5, HIGH);
+//      delay(35);
+//      digitalWrite(3, LOW);
+//      digitalWrite(4, LOW);
+//      digitalWrite(5, LOW);
+//
+//  }
+// Return the servo to the start position.
 
-      // Blink Lights during Engine Test
-      digitalWrite(3, ??);
-      digitalWrite(4, ??);
-      digitalWrite(5, ??);
-      delay(35);
-      digitalWrite(3, ??);
-      digitalWrite(4, ??);
-      digitalWrite(5, ??);
-      delay(35);
-      digitalWrite(3, ??);
-      digitalWrite(4, ??);
-      digitalWrite(5, ??);
-      delay(35);
-      digitalWrite(3, ??);
-      digitalWrite(4, ??);
-      digitalWrite(5, ??);
-  }
-  // Return the servo to the start position.
-  }
-
+//Serial.print("TEST FIRE WARP ENGINES\n");
+//  galacticEngine.write(0);  // tell servo to go back to degree 0.
+//  delay(15);  // waits 15ms for the servo to reach the position
+//  buttonCounter = 0;  // Reset buttonCounter to 0
+//}
 
 
 } // This brace closes the loop
